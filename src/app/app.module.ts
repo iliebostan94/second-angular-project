5738d8f32b0a26fd8eb3f80c9ee62ef40e65d15c
iliebostan94 <iliebostan@gmail.com>
2020-02-01 12:08:38 +0200

    git commit for recipe databinding Passing recipe data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent
-  ],
+   ],
   imports: [
     BrowserModule
   ],
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,16 +7,26 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output()recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A test recipe',
     'Just a test here for now',
      // tslint:disable-next-line:max-line-length
-     'https://images.unsplash.com/photo-1495546968767-f0573cca821e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60')
-  ];
+     'https://images.unsplash.com/photo-1495546968767-f0573cca821e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'),
+
+    new Recipe('A second cool recipe',
+    'Another description for recipe',
+     // tslint:disable-next-line:max-line-length
+     'https://img.taste.com.au/FGRFnJHc/taste/2016/11/prawn-and-egg-fried-rice-64324-1.jpeg')
+    ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
